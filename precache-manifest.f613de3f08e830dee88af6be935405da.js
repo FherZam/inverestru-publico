dcbef6df25e833e578f8bee4d98371ab1607657e
FherZam <84921254+FherZam@users.noreply.github.com>
2021-06-13 17:47:19 -0600

    Add files via upload

---
+++ precache-manifest.f613de3f08e830dee88af6be935405da.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271b4cb4fa6260d0bb060008b6685187",
+    "url": "/css/main.css"
+  },
+  {
+    "revision": "5e3f7193db91352c65b73692b497a33d",
+    "url": "/img/carousel1.5e3f7193.png"
+  },
+  {
+    "revision": "db55f3a49a3a9c7c51822a266df78a1a",
+    "url": "/img/carousel2.db55f3a4.png"
+  },
+  {
+    "revision": "2ea367ac98b9f5cbf2b1b9fe183b021f",
+    "url": "/img/carousel3.2ea367ac.png"
+  },
+  {
+    "revision": "c71647ab8b12e9106cf2dd03b3cd7b7a",
+    "url": "/img/gal1.c71647ab.jpg"
+  },
+  {
+    "revision": "34628d15f41739c59b04065d1a2beb4f",
+    "url": "/img/gal2.34628d15.jpg"
+  },
+  {
+    "revision": "26a454331dc3a9fff8a0f0cebc7d3b85",
+    "url": "/img/prod.26a45433.png"
+  },
+  {
+    "revision": "08e092dcab79f3d97e27975acd8f02f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5e29474f2e274ff36bf",
+    "url": "/js/about.a76567fa.js"
+  },
+  {
+    "revision": "2c5a9795338f80957318",
+    "url": "/js/app.5951c193.js"
+  },
+  {
+    "revision": "4431657b6dbe96a53fcc",
+    "url": "/js/chunk-vendors.2c150c08.js"
+  },
+  {
+    "revision": "3b21cf3c3790805363db",
+    "url": "/js/contact.26169e6e.js"
+  },
+  {
+    "revision": "39d728cfe6424882dcb3",
+    "url": "/js/galeria.f88411a1.js"
+  },
+  {
+    "revision": "8d2001ca9c1397f85ce6",
+    "url": "/js/nosotros.daecf52d.js"
+  },
+  {
+    "revision": "23a967ec475986e1c0b2",
+    "url": "/js/products.1dba32e4.js"
+  },
+  {
+    "revision": "95ef8b6b09576b60a45c",
+    "url": "/js/services.fa296bc5.js"
+  },
+  {
+    "revision": "0cca03cfa13fe4b24179598a2fefbc5f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
